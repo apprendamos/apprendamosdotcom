3d61fdc9cb98194d3ccae79b1709ed77dae17e77
Anthony Cueva <4nthonycuev4@gmail.com>
2023-01-07 11:38:15 -0500

    search page fixed

--- app/search/TopBar.tsx
+++ app/search/TopBar.tsx
@@ -1,69 +1,80 @@
 "use client";
 
-import { useState, useRef } from "react";
-import { BaseTopBar, PageBackButton } from "app/components";
-import {
-  CrossCircledIcon,
-} from "@radix-ui/react-icons";
-
+import { useEffect, useRef } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+
+import { BaseTopBar, PageBackButton } from "app/components";
 
 export default function Navbar() {
+  const inputRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const q = searchParams.get("q");
+  const q = searchParams.get("q") || "";
 
-  const [searchString, setSearchString] = useState(q || "");
-  const [lastSearchedString, setLastSearchedString] = useState("");
+  const searchString = useRef(q);
+  const lastSearchedString = useRef("");
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = q;
+    }
+  }, [q]);
 
   const timer: { current: NodeJS.Timeout | undefined } = useRef(undefined);
 
   const doneTyping = () => {
-    if (lastSearchedString === searchString) {
+    if (lastSearchedString.current === searchString.current) {
       return;
     }
 
-    if (searchString === "") {
+    if (searchString.current === "") {
+      lastSearchedString.current = "";
       router.push("/search");
     } else {
-      router.push(`/search?q=${searchString}`);
+      lastSearchedString.current = searchString.current;
+      router.push(`/search?q=${searchString.current}`);
     }
-
-    setLastSearchedString(searchString);
   };
 
   return (
-    <BaseTopBar>
+    <BaseTopBar className="space-x-2">
       <PageBackButton />
-      <input
-        value={searchString}
-        type="text"
-        placeholder="Search"
-        className="
+      <div className="flex space-x-2 w-full">
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search"
+          className="
+          grow
           rounded bg-transparent 
           px-4 py-1 m-0 
           outline-none ring-0 focus:ring-0
           border border-red-600/10 focus:border-red-600/10
         "
-        onKeyUp={(e) => {
-          clearTimeout(timer.current);
-          timer.current = setTimeout(doneTyping, 300);
-        }}
-        onKeyDown={(e) => {
-          clearTimeout(timer.current);
-        }}
-        onChange={(e) => setSearchString(e.target.value)}
-      />
-      <div className="flex space-x-4">
-        <button
-          onClick={() => {
-            setSearchString("");
-            router.push("/search");
+          onKeyUp={(e) => {
+            clearTimeout(timer.current);
+            timer.current = setTimeout(doneTyping, 300);
           }}
-        >
-          <CrossCircledIcon className="w-4 h-4 text-zinc-600 focus:text-zinc-500" />
-        </button>
+          onKeyDown={(e) => {
+            clearTimeout(timer.current);
+          }}
+          onChange={(e) => (searchString.current = e.target.value)}
+        />
+        <div className="flex space-x-4">
+          <button
+            onClick={() => {
+              searchString.current = "";
+              if (inputRef.current) {
+                inputRef.current.value = "";
+              }
+              router.push("/search");
+            }}
+          >
+            <CrossCircledIcon className="w-4 h-4 text-zinc-600 focus:text-zinc-500" />
+          </button>
+        </div>
       </div>
     </BaseTopBar>
   );
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,48 +1,66 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
+
 import { ArticleCardSmall, ProfileCard } from "app/components";
 import { ArticleType, ProfileType } from "types";
 
-export default function SearchPage(ctx: any) {
-  const {
-    searchParams: { q },
-  } = ctx;
+export default function SearchPage() {
+  const searchParams = useSearchParams();
+  const q = searchParams.get("q") || "";
 
-  const shouldFetch = q && q.length;
+  const shouldFetch = q.length > 0;
 
   const { data, error } = useSWR(shouldFetch ? `/api/explore?q=${q}` : null);
 
   const loading = !data && !error;
 
   if (q) {
-    if (loading) return <h1>Loading...</h1>;
+    if (loading)
+      return (
+        <div className="px-4 italic">
+          <h1>Loading result for {q}</h1>
+        </div>
+      );
+
+    if (!data.articles && !data.profiles) {
+      return (
+        <div className="px-4 italic">
+          <h1>No results found for {q}</h1>
+        </div>
+      );
+    }
 
     return (
-      <div className="flex flex-col space-y-4">
-        {data.articles && <div>
-          <h1 className="font-bold mb-2">Articles</h1>
-          <div className="flex flex-col space-y-4">
-            {data.articles.map((article: ArticleType) => (
-              <ArticleCardSmall key={article.id} {...article} />
-            ))}
+      <div className="flex flex-col space-y-4 px-4">
+        {data && data.articles && data.articles.length > 0 && (
+          <div>
+            <h1 className="font-bold mb-2">Articles</h1>
+            <div className="flex flex-col space-y-4">
+              {data.articles.map((article: ArticleType) => (
+                <ArticleCardSmall key={article.id} {...article} />
+              ))}
+            </div>
           </div>
-        </div> }
-        { data.profiles && <div>
-          <h1 className="font-bold mb-2">Users</h1>
-          <div className="flex flex-col space-y-4">
-            {data.profiles.map((profile: ProfileType) => (
-              <ProfileCard key={profile.username} {...profile} />
-            ))}
+        )}
+        {data && data.profiles && data.profiles.length > 0 && (
+          <div>
+            <h1 className="font-bold mb-2">Users</h1>
+            <div className="flex flex-col space-y-4">
+              {data.profiles.map((profile: ProfileType) => (
+                <ProfileCard key={profile.username} {...profile} />
+              ))}
+            </div>
           </div>
-        </div>}
+        )}
       </div>
     );
   }
 
   return (
-    <div className="min-h-144">
-      <h1>Try typing something...</h1>
+    <div className="px-4 italic">
+      <h1>Try typing something like "hello world"</h1>
     </div>
   );
 }
