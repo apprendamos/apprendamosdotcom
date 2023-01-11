34134692259ef6ada84fa7bb4ed0b425b2dc76d0
Anthony Cueva <4nthonycuev4@gmail.com>
2023-01-11 11:13:44 -0500

    xata talesb

--- app/(app)/profiles/[username]/followers/page.tsx
+++ app/(app)/profiles/[username]/followers/page.tsx
@@ -6,7 +6,7 @@ import { ProfileType } from "types";
 import { Profile } from "app/components";
 
 async function getFollowers(username: string) {
-  const page = await xata.db.follower_followee_rels
+  const page = await xata.db.profile_rels
     .filter("followee.username", username)
     .sort("creation_date", "desc")
     .select(["follower.*", "creation_date"])
